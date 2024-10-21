1260a1a669713ece41540643dcaef9f49e767a66
alberto marcos <amarcosarranz@gmail.com>
2024-10-21 02:17:49 +0200

    cambios en modelos

--- models/Product.model.js
+++ models/Product.model.js
@@ -3,14 +3,15 @@ const { Schema, model } = require("mongoose");
 const productSchema = new Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
-    ingredients: { type: String, required: true },
+    description: { type: String },
+    ingredients: { type: String },
     category: {
       type: String,
       enum: ["pizza", "pasta", "ensalada", "postre", "bebida"],
+      required: true,
     },
-    price: { type: Number, required: true },
-    img: { type: String, required: true },
+    price: { type: String, required: true },
+    img: { type: String },
   },
   {
     timestamps: true,
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const Product = require("../models/Product.model");
 
 const authRouter = require("./auth.routes");
 router.use("/auth", authRouter);
@@ -8,5 +9,13 @@ router.use("/admin", adminRouter);
 
 const userRouter = require("./user.routes");
 router.use("/user", userRouter);
+router.get("/product/:product", async (req, res, next) => {
+  try {
+    const response = await Product.find({ category: req.params.product });
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
